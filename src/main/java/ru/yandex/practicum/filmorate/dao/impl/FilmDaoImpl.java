1ec880734abc744e25fe4ca25e6c8b14a182056d
Vasily_Suchkov <MrGetFlash@yandex.ru>
2023-04-20 03:01:12 +0700

    Спринт 12, версия 3, fix 2 (Code/S)

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmDaoImpl.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.dao.*;
 import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.validation.Validators;
 
 import java.sql.ResultSet;
